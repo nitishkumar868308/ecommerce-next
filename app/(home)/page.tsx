587b34b3dc715297b310ef9c3605198d4688205b
Nitish Kumar <kumarnitish4384@gmail.com>
2025-02-11 16:02:09 +0530

    03-create-website-layout

---
+++ app/(home)/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Home() {
+  return (
+    <h1 className="h1-bold text-center p-10">Home Page Content</h1>
+  )
+}
---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,19 @@
+import { ShoppingCartIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function Menu() {
+    return (
+        <div className="flex justify-end">
+            <nav className="flex gap-3 w-full">
+                <Link href='/signin' className="flex items-center header-button">
+                   Hello, Sign in
+                </Link>
+
+                <Link href='/cart' className="header-button">
+                    <ShoppingCartIcon className="h-4 w-8" />
+                    <span>Card</span>
+                </Link>
+            </nav>
+        </div>
+    )
+}
