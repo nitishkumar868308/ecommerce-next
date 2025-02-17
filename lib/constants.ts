347d09fc734a810a29137afffcfeb62912616757
Nitish Kumar <kumarnitish4384@gmail.com>
2025-02-17 19:26:38 +0530

    payment-paypal

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,7 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NEXT_ECOMMERCE'
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+export const SENDER_NAME = process.env.SENDER_NAME || APP_NAME
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more'
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'An Ecommerce built with Next.js and MongoDB'
 export const APP_COPYRIGHT = process.env.NEXT_PUBLIC_APP_COPYRIGHT || `CopyRight @ 2025 ${APP_NAME}. All rights reserved`
