c00e6c3fe92ffd42c999306897d1ce56fea0ceb8
Nitish Kumar <kumarnitish4384@gmail.com>
2025-02-17 12:00:46 +0530

    sign-in-with-google

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -9,6 +9,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import CredentialsSignInForm from './credentials-signin-form'
 import { Button } from '@/components/ui/button'
 import { APP_NAME } from '@/lib/constants'
+import { GoogleSignInForm } from './google-signin-form'
 
 
 export const metadata: Metadata = {
@@ -38,6 +39,10 @@ export default async function SignInPage(props: {
                 <CardContent>
                     <div>
                         <CredentialsSignInForm />
+                    </div>
+                    <SeparatorWithOr />
+                    <div className='mt-4'>
+                        <GoogleSignInForm />
                     </div>
                 </CardContent>
             </Card>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -17,6 +17,10 @@ export const SignOut = async () => {
     redirect(redirectTo.redirect)
 }
 
+export const SignInWithGoogle = async () => {
+    await signIn('google')
+}
+
 export async function registerUser(userSignUp: IUserSignUp) {
     try {
         const user = await UserSignUpSchema.parseAsync({
