107a3eeeba9840d988e875d6a4f1daf584f330bf
Nitish Kumar <kumarnitish4384@gmail.com>
2025-02-18 14:59:15 +0530

    create-order-history-page

---
+++ components/shared/pagination.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { useRouter, useSearchParams } from 'next/navigation'
+import React from 'react'
+
+import { formUrlQuery } from '@/lib/utils'
+
+import { Button } from '../ui/button'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+
+type PaginationProps = {
+    page: number | string
+    totalPages: number
+    urlParamName?: string
+}
+
+const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
+    const router = useRouter()
+    const searchParams = useSearchParams()
+
+    const onClick = (btnType: string) => {
+        const pageValue = btnType === 'next' ? Number(page) + 1 : Number(page) - 1
+
+        const newUrl = formUrlQuery({
+            params: searchParams.toString(),
+            key: urlParamName || 'page',
+            value: pageValue.toString(),
+        })
+
+        router.push(newUrl, { scroll: true })
+    }
+
+    return (
+        <div className='flex items-center gap-2'>
+            <Button
+                size='lg'
+                variant='outline'
+                onClick={() => onClick('prev')}
+                disabled={Number(page) <= 1}
+                className='w-24'
+            >
+                <ChevronLeft /> Previous
+            </Button>
+            {/* {t('Search.Page')} {page} {t('Search.of')} {totalPages} */}
+            <Button
+                size='lg'
+                variant='outline'
+                onClick={() => onClick('next')}
+                disabled={Number(page) >= totalPages}
+                className='w-24'
+            >
+                Next <ChevronRight />
+            </Button>
+        </div>
+    )
+}
+
+export default Pagination
